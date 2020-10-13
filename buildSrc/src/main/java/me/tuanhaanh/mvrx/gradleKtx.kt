ca5871383c9112ea1f3e608cc5763411a0332b29
tuanictu97 <tuanhaanh@vccorp.vn>
2020-10-13 11:40:40 +0700

    first commit

---
+++ buildSrc/src/main/java/me/tuanhaanh/mvrx/gradleKtx.kt
@@ -0,0 +1,7 @@
+package me.tuanhaanh.mvrx
+
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.Project
+
+val Project.android: BaseExtension
+    get() = project.extensions.getByName("android") as BaseExtension
